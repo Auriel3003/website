0604e05c59efbd98d7f7306d4411f6385ff6fdd9
Sahil-at-Work <sahil.asawant30@gmail.com>
2025-02-19 17:58:13 +0530

    delete

--- src/data/data-analysis.js
+++
@@ -1,8 +0,0 @@
-import.meta.glob('./data-analysis/*.json', { as: 'json' }).then(modules => {
-    const projects = [];
-    for (const path in modules) {
-        const module = modules[path]();
-        projects.push(module.default);
-    }
-    export default projects;
-});
--- src/data/physics.js
+++
@@ -1,8 +0,0 @@
-import.meta.glob('./physics/*.json', { as: 'json' }).then(modules => {
-    const projects = [];
-    for (const path in modules) {
-        const module = modules[path]();
-        projects.push(module.default);
-    }
-    export default projects;
-});
